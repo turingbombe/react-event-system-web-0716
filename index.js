9cfc5f9d2a552c8108e96b6d06de2e8acc5a4c16
Charlie Cook <turingbombe@gmail.com>
2016-09-26 21:52:05 -0400

    Event stystem web

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const app = "I don't do much.";
+const React = require('react');
+
+class Tickler extends React.Component {
+  constructor() {
+    super();
+ 
+    this.tickle = this.tickle.bind(this);
+  }
+ 
+  tickle() {
+    console.log('Tee hee!');
+  }
+ 
+  render() {
+    return (
+      <button onClick={this.tickle}>Tickle me!</button>
+    );
+  }
+}
